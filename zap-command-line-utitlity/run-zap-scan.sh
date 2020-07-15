a7ed7a8b5eb796cee31761dd77e9d79e69a64505
ni3galave <nitin.galave@gmail.com>
2020-07-15 19:52:30 +0530

    Update run-zap-scan.sh

--- zap-command-line-utitlity/run-zap-scan.sh
+++ zap-command-line-utitlity/run-zap-scan.sh
@@ -4,6 +4,8 @@ export ZAP_PATH=/opt/asimily/zap/ZAP_2.9.0
 export ZAP_PORT=8090
 export ZAP_API_KEY=12345
 
+#!/usr/bin/env bash
+
 zap-cli open-url https://test-portal.asimily.com
 zap-cli spider --context-name defaultContext --user-name admin https://test-portal.asimily.com
 #zap-cli quick-scan -l Informational -s all -r -c Prod2May -u admin https://test-portal.asimily.com
